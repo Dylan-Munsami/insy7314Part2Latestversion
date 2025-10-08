2c4e6db70f305fd52d15965b7aa1f720e0ea0497
kamil maharaj <ST10068456@vcconnect.edu.za>
2025-10-09 01:26:53 +0200

    updated frontend

--- insy7314 part2 latest/international-payments/frontend/src/pages/CreatePayment.js
+++ insy7314 part2 latest/international-payments/frontend/src/pages/CreatePayment.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import { createPayment, sanitize } from "../services/api";
 import { useNavigate } from "react-router-dom";
