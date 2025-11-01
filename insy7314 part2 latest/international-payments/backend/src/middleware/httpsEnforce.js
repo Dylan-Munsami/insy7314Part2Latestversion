23b76c7074194448b2d3fda3c1b6edacaffc5112
Dylan-Munsami <130571261+Dylan-Munsami@users.noreply.github.com>
2025-11-01 18:34:29 +0200

    Resolve some conflicts

--- insy7314 part2 latest/international-payments/backend/src/middleware/httpsEnforce.js
+++ insy7314 part2 latest/international-payments/backend/src/middleware/httpsEnforce.js
@@ -1,13 +1,10 @@
 export default function enforceHttps(req, res, next) {
-  // Only enforce HTTPS in production
   if (
     process.env.NODE_ENV === "production" &&
     req.headers["x-forwarded-proto"] &&
     req.headers["x-forwarded-proto"] !== "https"
   ) {
-const host = "insy7314part2latestversion.onrender.com"; // production API domain
-return res.redirect(301, `https://${host}${req.url}`);
-   
+    return res.redirect(301, `https://${req.headers.host}${req.url}`);
   }
   next();
 }
